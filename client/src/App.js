40ed1b2a74f7153e240d3cfdfa90d960d2935554
LEE KyeongWon <leekyeongwon@LEEui-MacBookPro.local>
2020-08-31 22:54:12 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './App.css';
-import Customer from './components/Customer'
-import {Table, TableHead, TableBody, TableRow, TableCell} from '@material-ui/core';
-import {withStyles} from '@material-ui/core/styles';
+import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
+import { Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
@@ -21,62 +22,82 @@ const styles = theme => ({
   }
 });
 const App = (props) => {
-  const {classes} = props;
+  const { classes } = props;
   const [customers, setCustomers] = useState("");
   const [completed, setCompleted] = useState(0);
-
+  const [isLoad, setIsLoad] = useState(false);
+  
   useEffect(() => {
-    setTimeout(
-      progress , 20
-    );
-    callApi().then(res => setCustomers(res)).catch(err => console.log(err));
-  }, [completed]);
+    let complete = 0;
+    let timer = setInterval(() => {
+      if (complete >= 100) {
+        complete = 0
+      } else {
+        complete += 1;
+      }
+      setCompleted(complete);
+      if (isLoad) {
+        clearInterval(timer);
+      }
+    }, 20);
+    callApi().then(res => {
+      setCustomers(res);
+    }).
+      catch(err => console.log(err));
+  }, [isLoad]);
 
-  const callApi = async() => {
+  const callApi = async () => {
     const response = await fetch('/api/customers');
     const body = await response.json();
+    setIsLoad(true);
     return body;
   }
 
-  const progress = () => {
-    setCompleted(completed >= 100 ? 0 : completed + 1);
+  const sendPropsCallApi = () => {
+    callApi().then(res => {
+      setCustomers(res);
+    }).
+      catch(err => console.log(err));
   }
-  console.log(completed);
+
   return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        {
-        customers ? customers.map(c => {
-          return <Customer
-            key={c.id}
-            id={c.id}
-            image={c.image}
-            name={c.name}
-            birthday={c.birthday}
-            gender={c.gender}
-            job={c.job}
-          />
-        }):
-        <TableRow>
-          <TableCell colSpan = "6" align = "center">
-            <CircularProgress className={classes.progess} variant='determinate' value={completed}/>
-          </TableCell>
-        </TableRow>
-      }
-        </TableBody>
-      </Table>
-    </Paper>
+    <div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              customers ? customers.map(c => {
+                return <Customer
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              }) :
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress className={classes.progess} variant='determinate' value={completed} />
+                  </TableCell>
+                </TableRow>
+            }
+          </TableBody>
+        </Table>
+      </Paper>
+      <CustomerAdd sendPropsCallApi={sendPropsCallApi}/>
+    </div>
   );
 }
 
